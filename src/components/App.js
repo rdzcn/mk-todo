7fc5e4b7aad8f4f380b61e139f41c4263e2ea773
Arda Ozcan <ardaoezcan@gmail.com>
2019-05-25 22:36:26 +0200

    form submit is saved to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <input type="text">Add a todo</input>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    title: '',
+    todos: []
+  }
+
+  handleChange = event => {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const newTodo = {
+      title: this.state.title,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      todos: this.state.todos.concat(newTodo),
+      title: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+          <button type="submit">Add a todo</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
