d5b25377ce79532ba43ad5e1423ff5969add5576
Arda Ozcan <ardaoezcan@gmail.com>
2019-05-30 01:27:02 +0200

    moved everything to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,97 @@
-import React from 'react'
-import AddEditForm from './AddEditForm'
-import Todo from './Todo.js'
+import React from "react";
 
 class App extends React.Component {
   state = {
-    title: '',
-    todos: []
-  }
+    title: "",
+    todos: [],
+    isEditing: false,
+    indexEditing: ""
+  };
 
   handleChange = event => {
     this.setState({
       title: event.target.value
-    })
-  }
+    });
+  };
 
-  handleSubmit = newTodo => {
+  handleSubmit = event => {
+    event.preventDefault();
+    const id = this.state.todos.length + 1;
+    const todo = {
+      title: this.state.title,
+      completed: false,
+      id: id,
+      editable: false
+    };
     this.setState({
-      todos: this.state.todos.concat(newTodo),
-      title: ''
-    })
-  }
+      todos: this.state.todos.concat(todo),
+      title: ""
+    });
+  };
 
-  checkTodo = key => {
-    const { todos } = this.state
-    todos[key].completed = !todos[key].completed
-    this.setState({ todos })
-  }
+  handleCheckbox = event => {
+    const { todos } = this.state;
+    todos[event.target.name].completed = !todos[event.target.name].completed;
+    this.setState({ todos });
+  };
+
+  handleDelete = event => {
+    const { todos } = this.state;
+    todos.splice(event.target.name, 1);
+    this.setState({ todos });
+  };
+
+  handleEdit = event => {
+    const { todos } = this.state;
+    todos[event.target.name].editable = true;
+    this.setState({
+      isEditing: !this.state.isEditing,
+      indexEditing: event.target.name,
+      todos
+    });
+  };
 
   render() {
     return (
-      <div className="App">
-        <AddEditForm handleChange={this.handleChange} title={this.state.title} titleChange={this.handleChange} addTodo={this.handleSubmit} />
-        <Todo todos={this.state.todos} checkTodo={this.checkTodo} />
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add a todo</button>
+        </form>
+        <h2>Things to do</h2>
+        <ul>
+          {this.state.todos.map((item, index) => {
+            return item.editable ? (
+              <li>
+                <input type="text" name={index} value={item.title} />
+                <button type="button" name={index}>
+                  Save
+                </button>
+              </li>
+            ) : (
+              <li key={item.id}>
+                {item.title}
+                <input
+                  type="checkbox"
+                  name={index}
+                  checked={item.completed}
+                  onClick={this.handleCheckbox}
+                />
+                <button name={index} type="button" onClick={this.handleDelete}>
+                  Delete
+                </button>
+                <button name={index} type="button" onClick={this.handleEdit}>
+                  Edit
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
