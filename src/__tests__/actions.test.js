318ffdc23424f901f49b6b8d80ae98845463623f
Arda Ozcan <ardaoezcan@gmail.com>
2019-07-10 19:51:42 +0200

    updated test according to new addTodo(params)

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -21,31 +21,31 @@ dbAfter.data.todos["notes"] = dbAfter.data.todos["notes"].concat(todo)
 describe('testing addTodo', () => {
   test("persist() to be called when addTodo() is called", () => {
     const spy = jest.spyOn(state, "persist")
-    state.addTodo("hello", "notes", null, 11)
+    state.addTodo({title: "hello", category: "notes", dueDate: null, id: 11})
     expect(spy).toHaveBeenCalled()
   })
 
   test('null dueDate should add the task without a dueDate', () => {
-    state.addTodo("hello", "notes", null, 12)
+    state.addTodo({title: "hello", category: "notes", dueDate: null, id: 12})
     expect(db).toEqual(dbAfter)
   })
   
   test("A new todo must have a title, title cannot be empty", () => {
-    expect(state.addTodo(" ", "notes", "")).toBeFalsy()
-    expect(state.addTodo(null, "notes", null)).toBeFalsy()
+    expect(state.addTodo({title: " ", category: "notes", dueDate: ""})).toBeFalsy()
+    expect(state.addTodo({title: null, category: "notes", dueDate: null})).toBeFalsy()
   })
 })
 
 describe('testing toggleCompletionForTodo', () => {
   test("persist() to be called when toggleCompletionForTodo is called", () => {
     const spy = jest.spyOn(state, "persist")
-    state.addTodo("hello", "notes", null, 14)
+    state.addTodo({title: "hello", category: "notes", dueDate: null, id: 14})
     state.toggleCompletionForTodo(14)
     expect(spy).toHaveBeenCalled()
   })
   
   test("toggleCompletionForTodo should change todo.completed = false to true", () => {
-    state.addTodo("hello", "notes", null, 15)
+    state.addTodo({title: "hello", category: "notes", dueDate: null, id: 15})
     state.toggleCompletionForTodo(15)
     const addedTodo = state.data.todos["notes"].filter(todo => todo.id === 15)
     expect(addedTodo[0].completed).toBeTruthy()
