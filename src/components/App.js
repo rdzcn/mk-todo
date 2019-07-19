96447b330772d8f7c91ae626d5a94d82dd017fb1
Arda Ozcan <ardaoezcan@gmail.com>
2019-07-19 04:18:28 +0200

    App is a stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,22 @@ import React from 'react'
 import Content from './Content/Content'
 import Sidebar from './Sidebar/Sidebar'
 
-class App extends React.Component {
-	
-  sortTitle(todos) {
-    return todos.sort((a,b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase))
-  }
-
-  sortCreatedAt(todos) {
-    return todos.sort((a,b) => b.createdAt - a.createdAt)
-  }
-
-  sortModifiedAt(todos) {
-    return todos.sort((a,b) => b.modifiedAt - a.modifiedAt)
-  }
-
-  sortDueDate(todos) {
-    return todos.sort((a,b) => new Date(a.dueDate) - new Date(b.dueDate))
-  }
-	
-  render() {
-    const { router, repo } = this.props
-
-    return (
-      <React.Fragment>
-        <div className="app-container">
-          <header className="main header">
-            <h1>Your #1 Todo App</h1>
-          </header>
-          <aside className="main left">	
-            <Sidebar repo={repo} router={router} />
-          </aside>
-          <div className="main right">
-            <Content repo={repo} router={router} /> 
-          </div>
+const App = ({ repo, router }) => {
+  return (
+    <React.Fragment>
+      <div className="app-container">
+        <header className="main header">
+          <h1>Your #1 Todo App</h1>
+        </header>
+        <aside className="main left">	
+          <Sidebar repo={repo} router={router} />
+        </aside>
+        <div className="main right">
+          <Content repo={repo} router={router} /> 
         </div>
-      </React.Fragment>
-    )
-  }
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App
