623407897c23206b2cc867656aa733788768cf87
Arda Ozcan <ardaoezcan@gmail.com>
2019-07-19 04:09:34 +0200

    Input date should be parseable and match the format

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,16 +1,16 @@
 export function colorForDueDate(today, date) {
   const now = new Date(today)
   const dueDate = new Date(date)
-  const nowDay = now.getDate()
-  const nowMonth = now.getMonth()
-  const nowYear = now.getFullYear()
-  const dueDateDay = dueDate.getDate()
-  const dueDateMonth = dueDate.getMonth()
-  const dueDateYear = dueDate.getFullYear()
-	
-  if ((dueDateYear > nowYear) || 
-        (dueDateYear === nowYear && dueDateMonth > nowMonth) ||
-          (dueDateYear === nowYear && dueDateMonth === nowMonth && dueDateDay > nowDay)
+  
+  const [nowDay, nowMonth, nowYear] = [now.getDate(), now.getMonth(), now.getFullYear()]
+  const [dueDateDay, dueDateMonth, dueDateYear] = [dueDate.getDate(), dueDate.getMonth(), dueDate.getFullYear()]
+
+  if (isNaN(Date.parse(now)) || isNaN(Date.parse(dueDate))
+  ) {
+    return false
+  } if ((dueDateYear > nowYear) || 
+         (dueDateYear === nowYear && dueDateMonth > nowMonth) ||
+           (dueDateYear === nowYear && dueDateMonth === nowMonth && dueDateDay > nowDay)
   ) {
     return '#00f'
   } if (dueDateYear === nowYear &&
