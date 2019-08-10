ce962b8c3a539a9b9ce965c8d608bbf184e95a50
Arda Ozcan <ardaoezcan@gmail.com>
2019-08-10 21:12:29 +0200

    changed saveTodo

--- src/components/Content/SearchResults.js
+++ src/components/Content/SearchResults.js
@@ -4,7 +4,8 @@ import EditingTodo from './EditingTodo'
 
 const SearchResults = ({ state, router }) => {
 
-  const { search } = window.location
+  const { search } = router
+  const { data } = state
 
   return (
     <div className="todos">
@@ -13,15 +14,15 @@ const SearchResults = ({ state, router }) => {
       </button>
       <ul>
         { 
-          state.data.todos.map(todo => {
-            const regexSearchText = new RegExp(`\\b${search.replace('?', '')}`, 'gi')
+          data.todos.map(todo => {
+            const regexSearchText = new RegExp(`\\b${search}`, 'gi')
             const renderTodo = !!todo.title.match(regexSearchText)
-            const renderEditingTodo = !!(state.editingID === todo.id && todo.title.match(regexSearchText))
+            const renderEditingTodo = router.search === todo.id
 
             if (renderEditingTodo) {
-              return <EditingTodo key={todo.id} todo={todo} state={state} /> 
+              return <EditingTodo key={todo.id} todo={todo} router={router} state={state} /> 
             } else if (renderTodo) {
-              return <Todo key={todo.id} todo={todo} state={state} />
+              return <Todo key={todo.id} todo={todo} router={router} state={state} />
             } else {
               return null
             }
