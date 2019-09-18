0af4b3567dec7a264a2e09f4cd534cac57d0ea45
Arda Ozcan <ardaoezcan@gmail.com>
2019-09-19 00:46:56 +0200

    updated Content: unknown routes display 404

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,15 +4,19 @@ import SearchResults from './SearchResults'
  
 const Content = ({ state }) => {
   
+  const renderContent = state.route
   const renderSearch = state.route === 'search'
   let component, header
 
   if (renderSearch) {
     header = `Searching for: ${state.searchFor}`
     component = <SearchResults state={state} />
-  } else {
+  } else if (renderContent) {
     header = state.route
     component = <MainSection state={state} />
+  } else {
+    header = 'Not Found! You seem to be lost!'
+    component = <h1>404</h1>
   }
 
   return (
